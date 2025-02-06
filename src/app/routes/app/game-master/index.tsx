5f9cdae51192ebd3a14532bdf9f1051686e497c1
HerveDavid <david.herve.code@gmail.com>
2025-02-06 22:29:25 +0100

    wip

--- src/app/routes/app/game-master/index.tsx
+++ src/app/routes/app/game-master/index.tsx
@@ -1,11 +1,11 @@
+import { EditorLayout } from '@/components/layouts/editor';
 import Editor from '@/features/game-master/components/editor';
-import { useParams } from 'react-router';
 
 const GameMaster = () => {
   return (
-    <>
-      <Editor></Editor>
-    </>
+    <EditorLayout>
+      Hello
+    </EditorLayout>
   )
 };
 
