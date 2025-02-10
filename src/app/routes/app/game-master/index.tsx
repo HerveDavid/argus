3544bfcd3bcb8ca0f0a4e6fcf4e077c67bed72bd
hervedav <david.herve@rte-france.com>
2025-02-10 17:19:24 +0100

    feat: no animation

--- src/app/routes/app/game-master/index.tsx
+++ src/app/routes/app/game-master/index.tsx
@@ -1,6 +1,5 @@
 import { EditorLayout } from '@/components/layouts/editor';
 import Editor from '@/features/game-master/components/editor';
-import { TopMenuBar } from '@/features/top-menu-bar';
 
 const GameMaster = () => {
   return (
--- src/components/layouts/editor/index.tsx
+++ src/components/layouts/editor/index.tsx
@@ -16,14 +16,13 @@ type LayoutProps = {
 
 export const EditorLayout = ({ children }: LayoutProps) => {
   return (
-    <>
-      <TopMenuBar />
-
+    <div className="flex flex-col h-screen">
       <SidebarProvider>
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <Separator orientation="vertical" className="mr-2 h-4" />
+        <div className="flex flex-col flex-1">
+          <TopMenuBar />
+          <div className="border-b">
+            <div className="flex items-center gap-2 px-1">
+              <Separator orientation="vertical" className="h-4" />
               <Breadcrumb>
                 <BreadcrumbList>
                   <BreadcrumbItem className="hidden md:block">
@@ -36,12 +35,10 @@ export const EditorLayout = ({ children }: LayoutProps) => {
                 </BreadcrumbList>
               </Breadcrumb>
             </div>
-          </header>
-          <main className="flex flex-1 min-h-[calc(100vh-4rem)]">
-            {children}
-          </main>
-        </SidebarInset>
+          </div>
+          <main className="flex flex-1 overflow-hidden">{children}</main>
+        </div>
       </SidebarProvider>
-    </>
+    </div>
   );
 };
