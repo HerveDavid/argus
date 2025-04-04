adef51bd5d9aead8f4f3d004bcbe40f854513c0f
HerveDavid <david.herve.code@gmail.com>
2025-04-04 16:51:01 +0200

    feat: clean provider

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { HelmetProvider } from 'react-helmet-async';
-
 import { queryConfig } from '@/lib/react-query';
+import { useServerUrl } from '@/features/settings/url/hooks/use-server-url';
 
 type AppProviderProps = {
   children: React.ReactNode;
@@ -16,6 +16,16 @@ export const AppProvider = ({ children }: AppProviderProps) => {
         defaultOptions: queryConfig,
       }),
   );
+
+  const { refreshServerUrl } = useServerUrl();
+
+  React.useEffect(() => {
+    const initializeServerUrl = async () => {
+      await refreshServerUrl();
+    };
+
+    initializeServerUrl();
+  }, [refreshServerUrl]);
 
   return (
     <React.Suspense>
