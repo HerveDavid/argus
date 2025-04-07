c47ae8e144e481847a28327a3f491c0a89e93b62
HerveDavid <david.herve.code@gmail.com>
2025-04-07 11:16:18 +0200

    feat: build a provider for all services

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -3,7 +3,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { HelmetProvider } from 'react-helmet-async';
 import { queryConfig } from '@/lib/react-query';
-import { useServerUrl } from '@/features/settings/url/hooks/use-server-url';
+import {
+  RunWithServices,
+  ServicesProvider,
+} from '@/providers/services.provider';
+import { Effect } from 'effect';
+import { ServerUrlServiceTag } from '@/features/settings/url/services/server-url';
+import { ServerUrlError } from '@/features/settings/url/types/url.type';
 
 type AppProviderProps = {
   children: React.ReactNode;
@@ -17,23 +23,37 @@ export const AppProvider = ({ children }: AppProviderProps) => {
       }),
   );
 
-  const { refreshServerUrl } = useServerUrl();
+  const [isServicesReady, setIsServicesReady] = React.useState(false);
+  // Fonction pour initialiser le serveur URL et marquer l'application comme prête
+  const handleServicesInitialized = React.useCallback(
+    async (runWithServices: RunWithServices) => {
+      try {
+        // Initialiser l'URL du serveur directement ici
+        await runWithServices(
+          Effect.flatMap(ServerUrlServiceTag, (service) =>
+            service.getServerUrl(),
+          ) as Effect.Effect<any, ServerUrlError, any>,
+        );
 
-  React.useEffect(() => {
-    const initializeServerUrl = async () => {
-      await refreshServerUrl();
-    };
-
-    initializeServerUrl();
-  }, [refreshServerUrl]);
+        // Tout est prêt
+        setIsServicesReady(true);
+      } catch (error) {
+        console.error('Failed to initialize server URL:', error);
+        setIsServicesReady(true);
+      }
+    },
+    [],
+  );
 
   return (
     <React.Suspense>
       <HelmetProvider>
-        <QueryClientProvider client={queryClient}>
-          {import.meta.env.DEV && <ReactQueryDevtools />}
-          {children}
-        </QueryClientProvider>
+        <ServicesProvider onInitialized={handleServicesInitialized}>
+          <QueryClientProvider client={queryClient}>
+            {import.meta.env.DEV && <ReactQueryDevtools />}
+            {isServicesReady && children}
+          </QueryClientProvider>
+        </ServicesProvider>
       </HelmetProvider>
     </React.Suspense>
   );
