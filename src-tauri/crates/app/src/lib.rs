04df73bf4ff4d75808618f97093fb737dbdfb645
HerveDavid <david.herve.code@gmail.com>
2025-05-05 09:21:31 +0200

    fix: correct double log bug

--- src-tauri/crates/app/src/lib.rs
+++ src-tauri/crates/app/src/lib.rs
@@ -5,12 +5,10 @@ use tauri_plugin_shell::process::CommandChild;
 mod powsybl;
 mod settings;
 mod shared;
-// mod sidecars;
 mod state;
 
 use powsybl::commands::*;
 use settings::commands::*;
-// use sidecars::{commands::*, despawn_sidecar, spawn_and_monitor_sidecar};
 use state::AppState;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -23,13 +21,7 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
-        .plugin(
-            tauri_plugin_log::Builder::new()
-                .target(tauri_plugin_log::Target::new(
-                    tauri_plugin_log::TargetKind::Stdout,
-                ))
-                .build(),
-        )
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(sidecars::init())
         .invoke_handler(tauri::generate_handler![
             // Settings
