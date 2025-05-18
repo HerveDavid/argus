8b24290b9e5fdad0c6fafd69fe04cef1487bc557
HerveDavid <david.herve.code@gmail.com>
2025-05-18 17:01:39 +0200

    wip

--- src-tauri/src/shared/entities/iidm.rs
+++ src-tauri/src/shared/entities/iidm.rs
@@ -1,6 +1,11 @@
 use serde::{Deserialize, Serialize};
+use sqlx::{prelude::FromRow, Pool, Sqlite};
 
-#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub trait InsertExt {
+    async fn insert(&self, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error>;
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize, FromRow)]
 pub struct Substation {
     pub id: String,
     pub name: String,
@@ -14,7 +19,27 @@ pub struct Substations {
     pub substations: Vec<Substation>,
 }
 
-#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+impl InsertExt for Vec<Substation> {
+    async fn insert(&self, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
+        for substation in self {
+            sqlx::query_as::<_, Substation>(
+                "INSERT INTO substations (id, name, country, geo_tags, tag) 
+                 VALUES (?, ?, ?, ?, ?)
+                 RETURNING id, name, country, geo_tags, tag",
+            )
+            .bind(&substation.id)
+            .bind(&substation.name)
+            .bind(&substation.country)
+            .bind(&substation.geo_tags)
+            .fetch_optional(pool)
+            .await?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize, FromRow)]
 pub struct VoltageLevel {
     pub id: String,
     pub nominal_v: f64,
@@ -24,4 +49,23 @@ pub struct VoltageLevel {
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct VoltageLevels {
     pub voltage_levels: Vec<VoltageLevel>,
+}
+
+impl InsertExt for Vec<VoltageLevel> {
+    async fn insert(&self, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
+        for voltage_level in self {
+            sqlx::query_as::<_, VoltageLevel>(
+                "INSERT INTO voltage_levels (id, nominal_v, topology_kind) 
+                 VALUES (?, ?, ?)
+                 RETURNING id, nominal_v, topology_kind",
+            )
+            .bind(&voltage_level.id)
+            .bind(&voltage_level.nominal_v)
+            .bind(&voltage_level.topology_kind)
+            .fetch_optional(pool)
+            .await?;
+        }
+
+        Ok(())
+    }
 }
